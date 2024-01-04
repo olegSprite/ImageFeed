1c72de57d6dd553ce355db1af88214190092aa21
Oleg Spiridonov <spiri300@yandex.ru>
2024-01-04 10:11:23 +0500

    Сверстан экран с картинками, добавлены кастомные ячейки и таблица

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,16 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Олег Спиридонов on 03.01.2024.
+//
+
+import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var cellImageView: UIImageView!
+    @IBOutlet var dateLabel: UILabel!
+}
