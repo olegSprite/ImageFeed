5d12026e4f8fdae5fbb6dc457e04b419987a13ec
Oleg Spiridonov <spiri300@yandex.ru>
2024-01-07 09:59:31 +0300

    Почистил код, поправил шрифт

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -20,20 +20,21 @@ class ImagesListViewController: UIViewController {
         return formatter
     }()
     
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         tableView.delegate = self
         tableView.dataSource = self
-        
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
-    
-    // MARK: - Private function
-
 }
 
+// MARK: - TableViewDelegate
+
 extension ImagesListViewController: UITableViewDelegate {
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -49,6 +50,8 @@ extension ImagesListViewController: UITableViewDelegate {
     }
 }
 
+// MARK: - TableViewDataSource
+
 extension ImagesListViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -57,23 +60,23 @@ extension ImagesListViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
-                
-                guard let imageListCell = cell as? ImagesListCell else {
-                    return UITableViewCell()
-                }
-                
+        
+        guard let imageListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
+        }
+        
         configCell(for: imageListCell, with: indexPath)
-                return imageListCell
+        return imageListCell
     }
     
     func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
         guard let image = UIImage(named: photosName[indexPath.row]) else {
             return
         }
-
+        
         cell.cellImageView.image = image
         cell.dateLabel.text = dateFormatter.string(from: Date())
-
+        
         let isLiked = indexPath.row % 2 == 0
         let likeImage = isLiked ? UIImage(named: "Active") : UIImage(named: "No Active")
         cell.likeButton.setImage(likeImage, for: .normal)
