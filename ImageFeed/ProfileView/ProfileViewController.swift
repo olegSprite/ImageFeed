549d6f5ad4192de9ea4179cb3a672a11134694ab
Oleg Spiridonov <spiri300@yandex.ru>
2024-01-18 17:01:00 +0500

    Добавлена страница профиля и таббар

---
+++ ImageFeed/ProfileView/ProfileViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Олег Спиридонов on 18.01.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameLable: UILabel!
+    @IBOutlet weak var niknameLable: UILabel!
+    @IBOutlet weak var statusLable: UILabel!
+    
+    @IBOutlet weak var exitButton: UIButton!
+    
+    
+    
+    
+    @IBAction func exitAction(_ sender: Any) {
+    }
+}
