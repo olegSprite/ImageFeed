806ab6498aa62cc8fef504423cf95bbe0aedc2c9
Oleg Spiridonov <spiri300@yandex.ru>
2024-01-18 19:41:51 +0500

    Добавлена вью для отображения 1 картинки, добавлена навигация между таблицей и одной картинкой

---
+++ ImageFeed/ImagesList/SingleImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Олег Спиридонов on 18.01.2024.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
