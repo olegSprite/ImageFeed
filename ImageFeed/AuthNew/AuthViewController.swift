7bd3734e5309c08d05ea1e8cee7865c58e6043de
Oleg Spiridonov <spiri300@yandex.ru>
2024-02-03 20:52:38 +0500

    Потерял все изменения в проекте, переделал - новый комит (начал делать веб вью авторизации)

---
+++ ImageFeed/AuthNew/AuthViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Олег Спиридонов on 03.02.2024.
+//
+
+import Foundation
+import UIKit
+
+final class AuthViewController: UIViewController {
+    let identificator = "AuthViewController"
+}
--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+import Foundation
+
+let AccessKey = "<ваш Access Key>"
+let SecretKey = "<ваш Secret Key>"
+let RedirectURI = "<ваш Redirect URI>"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
