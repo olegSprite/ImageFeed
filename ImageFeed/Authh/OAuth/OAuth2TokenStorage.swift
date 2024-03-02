07433ba4eb814700c36c6324f49ed382978d7189
olegSprite <145563071+olegSprite@users.noreply.github.com>
2024-03-02 15:55:35 +0500

    подключил библеотеку кейчейна + заменил использование UD

--- ImageFeed/Authh/OAuth/OAuth2TokenStorage.swift
+++ ImageFeed/Authh/OAuth/OAuth2TokenStorage.swift
@@ -6,18 +6,24 @@
 //
 
 import Foundation
+import SwiftKeychainWrapper
 
 class OAuth2TokenStorage {
     
     let userDefaults = UserDefaults.standard
+    let keychainWrapper = KeychainWrapper.standard
     let tokenSave = "token"
     
     var token: String? {
         get {
-            return userDefaults.string(forKey: tokenSave)
+            keychainWrapper.string(forKey: tokenSave)
         }
         set {
-            userDefaults.set(newValue, forKey: tokenSave)
+            guard let newValue = newValue else { return }
+            let isSuccess = keychainWrapper.set(newValue, forKey: tokenSave)
+            guard isSuccess else {
+                fatalError("Ошибка сохранения токена")
+            }
         }
     }
 }
